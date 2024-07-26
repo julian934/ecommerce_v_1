49dc506a206fb36fd707d1ff8ff53fdfd04093d4
Julian Thatguy <jborner111@gmail.com>
2024-07-26 12:40:13 -0500

    file cleanup 2

--- src/app/public/page.js
+++ src/app/public/page.js
@@ -1,15 +1,13 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+//import { getServerSession } from "next-auth";
+//import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export default async function Protected(req){
-    const session=await getServerSession(authOptions)
-    console.log('Data',session)
+    //const session=await getServerSession(authOptions)
+    //console.log('Data',session)
     return(
         <div>
             <div>
-                {
-                    session !==null?<h1>Hi {session?.user.name}!</h1>:<a>Sign in</a>
-                }
+                
             </div>
         </div>
     )

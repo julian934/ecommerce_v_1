f8a862785f43ae13d2f5308c1bf54487237bb093
Julian Thatguy <jborner111@gmail.com>
2024-08-07 19:02:04 -0500

    opengraph optimized

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ import { Provider } from './provider'
 //import { getServerSession } from 'next-auth'
 const inter = Inter({ subsets: ['latin'] })
 import { appVals } from './lib/metadata/metaValues';
-
+import Head from 'next/head'
 export const metadata = appVals
 
 export default async function RootLayout({ children}) {
@@ -17,7 +17,14 @@ export default async function RootLayout({ children}) {
   //const session=await getServerSession(authOptions)
   return (
     <html lang="en">
-      
+      <Head>
+        <title>{appVals.title}</title>
+        <meta name="description" content={appVals.description} />
+        <meta property="og:title" content={appVals.title} />
+        <meta property="og:description" content={appVals.description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://nuthread-by-julian-cssl-1p9rqwnbx-julian934s-projects.vercel.app/" />
+      </Head>
       <StoreStateContextProvider>
       <body className={inter.className}>
         <Provider  >
